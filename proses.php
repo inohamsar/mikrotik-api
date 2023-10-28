8d47bc4c291ed2b7fef9bc1303ea720dfc916a9d
inohamsar <45508155+inohamsar@users.noreply.github.com>
2023-10-28 09:23:41 +0800

    change IP ke PMA

--- proses.php
+++ proses.php
@@ -2,9 +2,9 @@
 require("routeros_api.class.php");
 $API = new routeros_api();
 $API->debug = false;
-$username_mikrotik  = "xervio";
-$password_mikrotik  = "lead2707";
-$iphost_mikrotik    = "id3.labkom.us:6564";
+$username_mikrotik  = "admin";
+$password_mikrotik  = "pma220484";
+$iphost_mikrotik    = "id22.tunnel.my.id:4795";
 
 if($API->connect($iphost_mikrotik, $username_mikrotik, $password_mikrotik)){
 $username 	= $_POST['username'];
@@ -16,18 +16,18 @@ $mac	  	= $_POST['mac'];
 			"?name"     => $username,
 			));
 	if(count($cekuser)>0){
-		echo "<script>window.location='http://10.10.10.1/gagal.html'</script>";
+		echo "<script>window.location='http://192.168.40.1/gagal.html'</script>";
 	}else{
     $API->comm("/ip/hotspot/user/add", array(
 			"server"		=> "hotspot1",
-			"profile"		=> "TAMU",
+			"profile"		=> "Limitless",
 			"name"     		=> $username,
 			"password"		=> $password,
 			"mac-address"	=> $mac,		
 			"comment"		=> $nomor,
 			"limit-uptime"	=> "00:30:00",
 			));
-    echo "<script>window.location='http://10.10.10.1/login?username=".$username."&password=".$password."'</script>";
+    echo "<script>window.location='http://192.168.40.1/login?username=".$username."&password=".$password."'</script>";
 		}
 		$API->disconnect();
 	} 
